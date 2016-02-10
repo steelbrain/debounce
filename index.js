e4ad7e39b10b6d6b5332623a8554592b2c4366d0
steelbrain <de.steelbrain@gmail.com>
2016-02-10 23:08:14 +0000

    :new: Propogate  down

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 function debounce(callback, timeout, aggressive) {
   let timer = null
   let latestParameter
+  let latestThis
   function later() {
     timer = null
-    callback(latestParameter)
+    callback.call(latestThis, latestParameter)
   }
   return function debounced(parameter) {
     latestParameter = parameter
+    latestThis = this
     if (!aggressive || timer === null) {
       clearTimeout(timer)
       timer = setTimeout(later, timeout)
